70b8ffd9fcdebaca2bd9586d86e186ef8b94b6c5
McAppleRSky <mcapple@yandex.ru>
2020-10-21 18:12:44 +0300

    edit style

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <a class="phone" href="tel:=79788167486">978 816-74-86</a>
         <div class="footer_email">
-            name@mail.ru
+            mcapple@yandex.ru
         </div>
 
     </div>
--- index.php
+++ index.php
@@ -1,20 +1,18 @@
 <?php get_header(); ?>
-
-<main class="main">
-  <div class="container">
-    <div>
+  <main class="main">
+    <div class="container">
       <div class="main_title">
         Строительство домов и бань
-        из строганого и профилированного бруса
+        из строганного и профилированного бруса
       </div>
       <div class="main_text">
-        За 10 лет мы возвели более 250 домов и бань из бруса
+        За 10 лет мы возвели больше250 домов и бань из бруса
         ИМЕЕМ БОЛЬШОЙ ОПЫТ В СТРОИТЕЛЬСТВЕ
-        гарантируем качество и надежность всех построенных нами объектов
+        гарантируем качество и надежность всех аостроенных нами объектов
         ГАРАНТИЯ НА ДОМА И БАНИ ИЗ БРУСА 3 ГОДА
       </div>
       <div class="main_img">
-        <img src="<?php bloginfo( 'template_url' ); ?>/assets/img/main-img.png" alt="">
+        <img src="<?php bloginfo('template_url'); ?>/assets/img/main_img.png" alt="">
       </div>
       <div class="project">
         <div class="project_title">
@@ -22,109 +20,105 @@
         </div>
         <div class="project_item">
           <div class="project_name">
-            ДОМ №1
+            Дом №1
           </div>
-          <div class="project_size">
+          <div class="project_title">
             Размер дома: 6 х 6
           </div>
           <div class="project_area">
-            Общая площадь дома: 54 кв.м
+            Общая площадь дома: 54 кв. м
           </div>
           <div class="project_price">
-            Cnjbvjcnm 402 000 рублей
+            Стоимость - 402 000 рублей
           </div>
           <div class="project_images">
             <div class="project_images-item">
               <img src="img/project-1.jpg" alt="">
             </div>
-            <div class="progect_images-item">
+            <div class="project_images-item">
               <img src="img/project-2.jpg" alt="">
             </div>
           </div>
-        </div>
-        <div class="project_item">
           <div class="project_name">
-            ДОМ №1
+            Дом №1
           </div>
-          <div class="project_size">
+          <div class="project_title">
             Размер дома: 6 х 6
           </div>
           <div class="project_area">
-            Общая площадь дома: 54 кв.м
+            Общая площадь дома: 54 кв. м
           </div>
           <div class="project_price">
-            Cnjbvjcnm 402 000 рублей
+            Стоимость - 402 000 рублей
           </div>
           <div class="project_images">
             <div class="project_images-item">
               <img src="img/project-1.jpg" alt="">
             </div>
-            <div class="progect_images-item">
+            <div class="project_images-item">
               <img src="img/project-2.jpg" alt="">
             </div>
           </div>
-        </div>
-        <div class="project_item">
           <div class="project_name">
-            ДОМ №1
+            Дом №1
           </div>
-          <div class="project_size">
+          <div class="project_title">
             Размер дома: 6 х 6
           </div>
           <div class="project_area">
-            Общая площадь дома: 54 кв.м
+            Общая площадь дома: 54 кв. м
           </div>
           <div class="project_price">
-            Cnjbvjcnm 402 000 рублей
+            Стоимость - 402 000 рублей
           </div>
           <div class="project_images">
             <div class="project_images-item">
               <img src="img/project-1.jpg" alt="">
             </div>
-            <div class="progect_images-item">
+            <div class="project_images-item">
               <img src="img/project-2.jpg" alt="">
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="download">
-      <img src="<?php bloginfo( 'template_url' ); ?>/assets/img/home-download.png" alt="">
-      <div class="download_link">
-        <a href="#">СКАЧАТЬ ВЕСЬ КАТАЛОГ ДОМОВ</a>
-      </div>
-    </div>
-    <div class=container>
-      <div class="gallery">
+
+        <div class="download">
+          <img src="<?php bloginfo('template_url'); ?>/assets/img/home-download.png" alt="">
+          <a href="#">Скачать весь каталог домов</a>
+        </div>
+        <div class="container">
+
+        </div>
+
+        <div class="gallery"></div>
         <div class="gallery_title">
-          ФОТОГРАФИИ НАШИХ РАБОТ]
+          ФОТОГРАФИИ НАШИХ ДОМОВ
         </div>
         <div class="gallery_text">
           некоторые фотографии построенных нами домов и бань из бруса
         </div>
         <div class="gallery_inner">
           <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
-          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
-          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
-          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
-          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
-          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
-          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
-          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
-          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
-          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
-          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
-          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
-          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
-          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
-          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
-          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
+          <a href="img/item-2.jpg"><img src="img/item-2.jpg" alt=""></a>
+          <a href="img/item-3.jpg"><img src="img/item-3.jpg" alt=""></a>
+          <a href="img/item-4.jpg"><img src="img/item-4.jpg" alt=""></a>
+          <a href="img/item-5.jpg"><img src="img/item-5.jpg" alt=""></a>
+          <a href="img/item-6.jpg"><img src="img/item-6.jpg" alt=""></a>
+          <a href="img/item-7.jpg"><img src="img/item-7.jpg" alt=""></a>
+          <a href="img/item-8.jpg"><img src="img/item-8.jpg" alt=""></a>
+          <a href="img/item-9.jpg"><img src="img/item-9.jpg" alt=""></a>
+          <a href="img/item-10.jpg"><img src="img/item-10.jpg" alt=""></a>
+          <a href="img/item-11.jpg"><img src="img/item-11.jpg" alt=""></a>
+          <a href="img/item-12.jpg"><img src="img/item-12.jpg" alt=""></a>
+          <a href="img/item-13.jpg"><img src="img/item-13.jpg" alt=""></a>
+          <a href="img/item-14.jpg"><img src="img/item-14.jpg" alt=""></a>
+          <a href="img/item-15.jpg"><img src="img/item-15.jpg" alt=""></a>
+          <a href="img/item-16.jpg"><img src="img/item-16.jpg" alt=""></a>
         </div>
       </div>
       <div class="main_img">
-        <img src="<?php bloginfo( 'template_uri' ) ?>/assets/img/main-img.png" alt="">
+        <img src="<?php bloginfo('template_url'); ?>/assets/img/main_img.png" alt="">
       </div>
     </div>
-</main>
-
+  </main>
 <?php getfooter(); ?>
